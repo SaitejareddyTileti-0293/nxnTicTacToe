f560e8f0ea99d4d05447748a1dea5ccdb482cf32
SaitejareddyTileti-0293 <saitejareddytileti@gmail.com>
2020-08-29 15:56:57 +0530

    Updates

---
+++ precache-manifest.68ea106359c6e244a06f787d3c9a92aa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829cb79d7dc518e7f192a400f3bc6bc2",
+    "url": "/SaitejareddyTileti-0293/nxnTicTacToe/index.html"
+  },
+  {
+    "revision": "777e2212136c1e314732",
+    "url": "/SaitejareddyTileti-0293/nxnTicTacToe/static/css/main.182da46d.chunk.css"
+  },
+  {
+    "revision": "947402580317f5ae2948",
+    "url": "/SaitejareddyTileti-0293/nxnTicTacToe/static/js/2.981d4f54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SaitejareddyTileti-0293/nxnTicTacToe/static/js/2.981d4f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "777e2212136c1e314732",
+    "url": "/SaitejareddyTileti-0293/nxnTicTacToe/static/js/main.aa43fb0c.chunk.js"
+  },
+  {
+    "revision": "792aa42237a0c662f94a",
+    "url": "/SaitejareddyTileti-0293/nxnTicTacToe/static/js/runtime-main.e3f3259a.js"
+  },
+  {
+    "revision": "14587b5af9621f5fb35ca6a271661a2f",
+    "url": "/SaitejareddyTileti-0293/nxnTicTacToe/static/media/bronze-trophy.14587b5a.svg"
+  },
+  {
+    "revision": "8592cf61803a57eaa01822e3b1443936",
+    "url": "/SaitejareddyTileti-0293/nxnTicTacToe/static/media/dolphin.8592cf61.svg"
+  },
+  {
+    "revision": "d4ac01cfd2867c1b1c59d5476932e3de",
+    "url": "/SaitejareddyTileti-0293/nxnTicTacToe/static/media/golden-globe.d4ac01cf.svg"
+  },
+  {
+    "revision": "ea73e7af4970adf8e4a4c0d4b9589c1b",
+    "url": "/SaitejareddyTileti-0293/nxnTicTacToe/static/media/silver-cup.ea73e7af.svg"
+  }
+]);
